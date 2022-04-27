4c5e08b7aa63729c4bba6c8b425b7604efdcd568
SuelaCark <77395265+SuelaCark@users.noreply.github.com>
2022-04-27 17:08:49 +0300

    Update GamePanel.java
    Obstacles added + YES/NO button added for restarting the game

--- V2/GamePanel.java
+++ V2/GamePanel.java
@@ -1,30 +1,35 @@
 package V2;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.awt.event.*;
 import java.awt.*;
 import java.util.Random;
 
 
 public class GamePanel extends JPanel implements ActionListener {
-    static final int SCREEN_WIDTH=600;
-    static final int SCREEN_HEIGHT=600;
-    static final int UNIT_SIZE=25;
-    static final int GAME_UNIT= SCREEN_HEIGHT*SCREEN_HEIGHT/UNIT_SIZE;
-    static final int DELAY=75;
-    final int[] x=new int[GAME_UNIT];
-    final int[] x1=new int[GAME_UNIT];
-    final int[] y=new int[GAME_UNIT];
-    final int[] y1=new int[GAME_UNIT];
-    int bodyparts=3;
-    int bodyparts1=3;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNIT = SCREEN_HEIGHT*SCREEN_HEIGHT/UNIT_SIZE;
+    static final int DELAY = 100;
+    final int[] x = new int[GAME_UNIT];
+    final int[] x1 = new int[GAME_UNIT];
+    final int[] y = new int[GAME_UNIT];
+    final int[] y1 = new int[GAME_UNIT];
+    int bodyparts = 3;
+    int bodyparts1 = 3;
     int foodEaten;
     int foodEaten1;
     int appleX;
     int appleY;
+    int obstacleX;
+    int obstacleY;
+    int obstacleX1;
+    int obstacleY1;
     char direction='R';
     char direction1='D';
-    static boolean running=false;
+    static boolean running = false;
     Timer timer;
     Random random;
 
@@ -36,21 +41,19 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
-
-
     }
 
     public void drawSnake(int[] x, int[] y, int bodyparts, Graphics g, String color){
         for(int i=0; i<bodyparts; i++){
             if(i==0){
-                if (color=="GREEN")
+                if (color.equals("GREEN"))
                 g.setColor(Color.GREEN);
                 else 
                 g.setColor(Color.RED);
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             }
             else{
-                if (color=="GREEN")
+                if (color.equals("GREEN"))
                 g.setColor(new Color(45,180,0));
                 else 
                 g.setColor(new Color(180,40,0));
@@ -62,18 +65,21 @@ public class GamePanel extends JPanel implements ActionListener {
     public void startGame(){
         running=true;
         newFood();
-        timer=new Timer(DELAY, this);
+        timer = new Timer(DELAY, this);
         timer.start();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString("Score: " + foodEaten, 20, 40);
     }
     public void draw(Graphics g){
 
         if(running){
-            for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
+            for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }
@@ -82,15 +88,26 @@ public class GamePanel extends JPanel implements ActionListener {
 
             drawSnake(x, y, bodyparts, g, "GREEN");
             drawSnake(x1, y1, bodyparts1, g, "RED");
+            
+            if (foodEaten >= 10){
+                g.setColor(Color.cyan);
+                g.fillRect(obstacleX, obstacleY, UNIT_SIZE, UNIT_SIZE);
+                g.fillRect(obstacleX1, obstacleY1, UNIT_SIZE, UNIT_SIZE);
+            }
         }
         else{gameOver(g);}
-        
     }
+    
+    public void newObstacle(){
+        obstacleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        obstacleY = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        obstacleX1 = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        obstacleY1 = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+    }
+    
     public void newFood(){
         appleX=random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
         appleY=random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
-        
-
     }
     public void move(){
         for(int i=bodyparts; i>0; i--){
@@ -139,55 +156,73 @@ public class GamePanel extends JPanel implements ActionListener {
             System.out.println(bodyparts);
             this.foodEaten++;
             bodyparts++;
+            newObstacle();
             newFood();
         }
         return bodyparts;
     }
     public void checkFood(){
-        
-        bodyparts=checkSnakeFood(x, y, bodyparts);
-        bodyparts1=checkSnakeFood(x1, y1, bodyparts1);
+        bodyparts = checkSnakeFood(x, y, bodyparts);
+        bodyparts1 = checkSnakeFood(x1, y1, bodyparts1);
     }
 
     public void checkSnakeCollisionBody(int[] x, int[] y, int bodyparts){
-        for(int i=bodyparts; i>0; i--){
+        for(int i = bodyparts; i > 0; i--){
             if((x[0]==x[i]) && (y[0]==y[i])){
-                running=false;
+                running = false;
             }
+        }
+    }
+    
+    public void checkObstacleCollisionBody(int[] x, int[] y, int bodyparts){
+        if((x[0]==obstacleX) && (y[0]==obstacleY)){
+            running = false;
+        }
+        if((x[0]==obstacleX1) && (y[0]==obstacleY1)){
+            running = false;
         }
     }
 
     public void checkSnakeCollisionWall(int[] x, int[] y, int bodyparts){
-        if(x[0]<0){
-            running=false;
+        if(x[0] < 0){
+            running = false;
         }
-        if(x[0]>SCREEN_WIDTH-1){
-            running=false;
+        if(x[0] > SCREEN_WIDTH-1){
+            running = false;
         }
-        if(y[0]<0){
-            running=false;
+        if(y[0] < 0){
+            running = false;
         }
-        if(y[0]>SCREEN_HEIGHT-1){
-            running=false;
+        if(y[0] > SCREEN_HEIGHT-1){
+            running = false;
         }
-
     }
 
     public void checkCollision(){
 
-        checkSnakeCollisionBody(x,y,bodyparts);
-        checkSnakeCollisionBody(x1,y1,bodyparts1);
+        checkSnakeCollisionBody(x, y, bodyparts);
+        checkSnakeCollisionBody(x1, y1, bodyparts1);
         checkSnakeCollisionWall(x, y, bodyparts);
         checkSnakeCollisionWall(x1, y1, bodyparts1);
+        
+        checkObstacleCollisionBody(x, y, bodyparts);
+        checkObstacleCollisionBody(x1, y1, bodyparts1);
 
-        if(running==false){
+        if(!running){
             timer.stop(); 
         }
     }
     
     public void gameOver(Graphics g){
-        JOptionPane.showMessageDialog(null, "Gamer Over! You Scored " + (foodEaten)+ " Points!");
-        System.exit(0);
+        int result = JOptionPane.showConfirmDialog(null, "Gamer Over! You Scored " + (foodEaten)+ " Points!"
+                + "\n Would you like to restart?", "Game Over" , JOptionPane.YES_NO_OPTION);
+
+        if (result  == JOptionPane.YES_OPTION) {
+            // YES button was pressed
+            new GameFrame();
+        } else {
+            System.exit(0);
+        }
     }
 
     public class MyKeyAdapter extends KeyAdapter{
@@ -234,10 +269,8 @@ public class GamePanel extends JPanel implements ActionListener {
                         direction1='D';
                         break;
                     }
-                
             }
         }
-    
     }
 
     @Override
@@ -248,6 +281,5 @@ public class GamePanel extends JPanel implements ActionListener {
             checkCollision();
         }
         repaint();        
-    }
-    
+    }  
 }
